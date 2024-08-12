49570791e4cc830ef0404249ba5a282d6ccd3989
Dadan28 <dadannurmaulana@gmail.com>
2024-08-13 06:07:32 +0700

    Edit About

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,20 +4,17 @@
         <div class="flex justify-between px-6 mx-auto max-w-screen-xl ">
             <article class="mx-auto w-full max-w-2xl format sm:format-base lg:format-lg format-blue dark:format-invert">
                 <h2>LOKER SUBANG</h2>
-                <p>Loker Subang adalah situs informasi lowongan kerja subang dimana setiap harinya selalu update Info loker
-                    subang terbaru dan terpercaya.</p>
+                <p>Loker Subang adalah situs informasi lowongan kerja subang yang didirikan pada tahun 2020 yang berawal dari Covid 19 dimana informasi lowongan kerja sulit didapatkan banyak dari kita yang di rumahkan. Sehingga lokersubang.com hadir untuk membantu teman-teman terutama untuk Wilayah Kota Subang dan Sekitarnya untuk mendapatkan informasi lowongan kerja terpercaya.</p>
                 <p>Sumber informasi didapatkan dari beberapa referensi seperti situs resmi perusahaan, linkedin, jobstreet,
                     instagram, dan media sosial lainnya.</p>
                 <p>Setiap postingan yang dibuat loker subang selalu melampirkan sumbernya. Sehingga teman-teman pencaker bisa
                     lebih yakin dan bisa mendapatkan informasi lowongan kerja yang sebenarnya.</p>
                 <p>Selain itu, loker subang juga membagikan informasi lowongan kerja dari beberapa wilayah lainnya seperti
                     Indramayu, Karawang, Cirebon, Purwakarta, Bandung, Majalengka, Bekasi dan kota lainnya. </p>
-                <p>Informasi lowongan kerja yang berasal dari selain wilayah Subang akan dikategorikan sebagai Lowongan Kerja
-                    Luar Kota.</p>
+                <p>Informasi lowongan kerja yang berasal dari selain wilayah Subang akan dikategorikan sebagai Lowongan Kerja.</p>
                 <p>Dan bagi siapa saja yang ingin membagikan informasi lowongan kerja atau ingin mengiklankan lowongan kerja
-                    bisa kontak admin yang tertera. </p>
-                <p>Membuat postingan di lokersubang.com tidak dipungut biaya apapun alias Gratis 100%. Buat perusahaan atau
-                    teman-teman yang ingin membagikan info loker subang bisa juga melalui media sosial kami. </p>
+                    bisa menghubungi menu Kontak. </p>
+                <p>Membuat postingan di lokersubang.com tidak dipungut biaya apapun alias Gratis 100%. </p>
                 <p>Berawal dari banyaknya website informasi lowongan kerja yang memberikan informasi dari sumber tidak jelas.
                     Sehingga membuat para pencari kerja tidak bisa mendapatkan informasi yang mereka harapkan.</p>
                 <p>Tidak sedikit juga banyak oknum yang mengatasnamakan perusahaan untuk kepentingan bisnis. Sehingga banyak
